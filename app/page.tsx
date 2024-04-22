49a6eaeab2f735e97b5d58511762d54ab27210f2
Rafa Canosa <rafacv23@gmail.com>
2024-04-22 15:11:01 +0200

    extract content into component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import { Input } from "@nextui-org/input"
+import SendUrlForm from "@/app/components/sendUrlForm"
+import { getData } from "@/app/utils/getData"
 
-export default function Home() {
+export default async function Home() {
+  await getData()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        URL shortener
-        <Input />
+        <SendUrlForm />
       </div>
     </main>
   )
