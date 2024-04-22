c6a46f0c62eab632d6579a3728c12430b2e2eefc
Rafa Canosa <rafacv23@gmail.com>
2024-04-22 14:18:59 +0200

    first commit

---
+++ app/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function page() {
+  return (
+    <div>
+      Esta es la página que se va a encargar de redirigir a la gente hacia la
+      url real
+    </div>
+  )
+}
