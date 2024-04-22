43d3ec7bb95bf558563795e1caf84e319c293ed7
Rafa Canosa <rafacv23@gmail.com>
2024-04-22 17:08:15 +0200

    get short url by large url

---
+++ src/lib/getShortUrl.ts
@@ -0,0 +1,13 @@
+import { supabase } from "./supabase"
+
+export async function getShortUrl(large_url: string) {
+  let { data: url, error } = await supabase
+    .from("urls")
+    .select("short_url")
+    .eq("large_url", large_url)
+
+  if (error) {
+    console.log(error)
+  }
+  return url
+}
