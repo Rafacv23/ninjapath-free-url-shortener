8e29211f426b689f02fc849c040d060a24482437
Rafa Canosa <rafacv23@gmail.com>
2024-04-22 16:19:55 +0200

    astro

--- app/utils/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient, SupabaseClient } from "@supabase/supabase-js"
-
-const supabaseUrl: string = process.env.SUPABASE_URL!
-const supabaseKey: string = process.env.SUPABASE_KEY!
-
-export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey)
