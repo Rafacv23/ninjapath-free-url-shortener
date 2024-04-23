5cf810e7ed9351de11ba3f018f1f74b40f5df687
Rafa Canosa <rafacv23@gmail.com>
2024-04-23 17:04:58 +0200

    change name env var

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,6 @@
 interface ImportMetaEnv {
-  readonly SUPABASE_URL: string
-  readonly SUPABASE_ANON_KEY: string
+  readonly PUBLIC_SUPABASE_URL: string
+  readonly PUBLIC_SUPABASE_ANON_KEY: string
 }
 
 interface ImportMeta {
