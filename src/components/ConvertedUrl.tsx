41f063111ab0c8eed5026629fbf0384e4044a568
Rafa Canosa <rafacv23@gmail.com>
2024-04-23 17:17:04 +0200

    remove comments

--- src/components/ConvertedUrl.tsx
+++ src/components/ConvertedUrl.tsx
@@ -3,7 +3,6 @@ import { copyUrlToClipboard } from "../utils/copyUrlToClipboard"
 import type { convertedUrl } from "../utils/definitions"
 
 const ConvertedUrl = ({ convertedUrl }: convertedUrl) => {
-  // Usar React.FC con la interfaz definida para los props
   return (
     <div>
       <h4>{convertedUrl}</h4>
