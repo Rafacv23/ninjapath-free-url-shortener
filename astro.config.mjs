1ab2f455624da93cb617a61a59c2436bb84352d4
Rafa Canosa <rafacv23@gmail.com>
2024-04-24 12:46:17 +0200

    linter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
-
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config"
+import vercel from "@astrojs/vercel/serverless"
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [react()]
-});
+  integrations: [react()],
+})
