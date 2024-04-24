f7d9d4a84bbe104a06bfe917ab3ccb7f25d95b07
Rafa Canosa <rafacv23@gmail.com>
2024-04-24 12:30:08 +0200

    added title and description

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,2 +1,6 @@
 export const SITE_URL: string = "https://ninjapath.vercel.app"
 export const SITE_NAME: string = "Ninja Path"
+export const SITE_EN_TITLE: string = `${SITE_NAME} | Free URL shortener`
+export const SITE_ES_TITLE: string = `${SITE_NAME} | Acortador de URLs gratis`
+export const SITE_EN_DESCRIPTION: string = `${SITE_NAME} it's a free URL shortener. You can make your own URLs in just one click.`
+export const SITE_ES_DESCRIPTION: string = `${SITE_NAME} Es un acortador de URLs gratuito. Te permite crear tus propias URLs en tan sólo un click.`
