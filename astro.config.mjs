faa2de0d508e05aec2e6aa695bf758d44f266901
Rafa Canosa <rafacv23@gmail.com>
2024-06-05 15:51:57 +0200

    added site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
-import react from "@astrojs/react";
+import { defineConfig } from "astro/config"
+import vercel from "@astrojs/vercel/serverless"
+import react from "@astrojs/react"
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from "@astrojs/sitemap"
+import { SITE_URL } from "./src/utils/constants"
 
 // https://astro.build/config
 export default defineConfig({
+  site: SITE_URL,
   output: "server",
   adapter: vercel(),
-  integrations: [react(), sitemap()]
-});
+  integrations: [react(), sitemap()],
+})
