39ed174e7446e1a95d93aaa91491b869da0186cc
Rafa Canosa <rafacv23@gmail.com>
2024-06-18 14:54:11 +0200

    new algorithm to set the converted urls

--- src/utils/generateShortUrl.ts
+++ src/utils/generateShortUrl.ts
@@ -1,3 +1,13 @@
-export const generateShortUrl = (): number => {
-  return Math.floor(Math.random() * 1000000) + 1
+export const generateShortUrl = (): string => {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+  const length = 8
+  let randomString = ""
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length)
+    randomString += characters[randomIndex]
+  }
+
+  return randomString
 }
