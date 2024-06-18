0c2ea4f067fa9798d3496f438de6de08c5089c5f
Rafa Canosa <rafacv23@gmail.com>
2024-06-18 14:20:26 +0200

    added new componentes like shareqr and sharerss

--- src/components/ConvertedUrl.tsx
+++ src/components/ConvertedUrl.tsx
@@ -1,12 +1,37 @@
 import React from "react"
 import { copyUrlToClipboard } from "../utils/copyUrlToClipboard"
 import type { convertedUrl } from "../utils/definitions"
+import "../styles/ShareBtns.css"
+import { IconBoxArrowUpRight } from "../utils/icons"
+import ShareRRSS from "./share/ShareRRSS"
+import ShareQR from "./share/ShareQR"
 
 const ConvertedUrl = ({ convertedUrl }: convertedUrl) => {
   return (
     <div>
       <h4>{convertedUrl}</h4>
-      <button onClick={() => copyUrlToClipboard(convertedUrl)}>Copy</button>
+      <div className="btns">
+        <a
+          data-tooltip="Visit URL"
+          data-placement="bottom"
+          className="contrast"
+          href={convertedUrl}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <IconBoxArrowUpRight />
+        </a>
+        <ShareQR convertedUrl={convertedUrl} />
+        <ShareRRSS convertedUrl={convertedUrl} />
+        <button
+          data-tooltip="Copy to clipboard"
+          className="contrast"
+          data-placement="bottom"
+          onClick={() => copyUrlToClipboard(convertedUrl)}
+        >
+          Copy
+        </button>
+      </div>
     </div>
   )
 }
--- src/components/es/ConvertedUrl.tsx
+++ src/components/es/ConvertedUrl.tsx
@@ -1,12 +1,37 @@
 import React from "react"
 import { copyUrlToClipboard } from "../../utils/copyUrlToClipboard"
 import type { convertedUrl } from "../../utils/definitions"
+import "../../styles/ShareBtns.css"
+import ShareRRSS from "./share/ShareRRSS"
+import { IconBoxArrowUpRight } from "../../utils/icons"
+import ShareQR from "./share/ShareQR"
 
 export default function ConvertedUrl({ convertedUrl }: convertedUrl) {
   return (
     <div>
       <h4>{convertedUrl}</h4>
-      <button onClick={() => copyUrlToClipboard(convertedUrl)}>Copiar</button>
+      <div className="btns">
+        <a
+          data-tooltip="Visitar URL"
+          data-placement="bottom"
+          className="contrast"
+          href={convertedUrl}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <IconBoxArrowUpRight />
+        </a>
+        <ShareQR convertedUrl={convertedUrl} />
+        <ShareRRSS convertedUrl={convertedUrl} />
+        <button
+          data-tooltip="Copiar al portapapeles"
+          className="contrast"
+          data-placement="bottom"
+          onClick={() => copyUrlToClipboard(convertedUrl)}
+        >
+          Copiar
+        </button>
+      </div>
     </div>
   )
 }
