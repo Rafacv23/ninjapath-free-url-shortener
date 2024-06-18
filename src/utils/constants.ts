40323a28deac8d7544e1a2a0134ea64425fab9b6
Rafa Canosa <rafacv23@gmail.com>
2024-06-18 14:35:58 +0200

    NEW IMG

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,3 +5,4 @@ export const SITE_ES_TITLE: string = `${SITE_NAME} | Acortador de URLs gratis`
 export const SITE_EN_DESCRIPTION: string = `${SITE_NAME} it's a free URL shortener. You can make your own URLs in just one click.`
 export const SITE_ES_DESCRIPTION: string = `${SITE_NAME} Es un acortador de URLs gratuito. Te permite crear tus propias URLs en tan sólo un click.`
 export const SITE_IMG = "./ninjapath-img.avif"
+export const PUBLIC_SITE_IMG = "https://i.imgur.com/CTqiDSE.png"
