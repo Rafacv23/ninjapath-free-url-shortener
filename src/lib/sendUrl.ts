fcbc4a6b8d94c2ef1e2b39fd60ba1760043e69e8
Rafa Canosa <rafacv23@gmail.com>
2024-06-19 14:18:02 +0200

    added zustand state

--- src/lib/sendUrl.ts
+++ src/lib/sendUrl.ts
@@ -3,6 +3,7 @@ import { getShortUrl } from "./getShortUrl.ts"
 import { generateShortUrl } from "../utils/generateShortUrl.ts"
 import { findExistingLargeUrl } from "./findExistingLargeUrl.ts"
 import { SITE_URL } from "../utils/constants.ts"
+import useUrlStore from "./useUrlStore.ts"
 
 export async function sendUrl(
   url: string,
@@ -10,39 +11,29 @@ export async function sendUrl(
 ): Promise<string | Response> {
   try {
     const urlExists = await findExistingLargeUrl(url)
+    const date = new Date().toISOString()
+    const { addUrl } = useUrlStore.getState()
     if (urlExists) {
-      // Si la URL ya existe en la base de datos, devuelve la URL corta existente
       console.log("La URL ya existe en la base de datos")
       const urls = await getShortUrl(url)
-      const short_url = urls?.map((url) => url.short_url)
-      return `${SITE_URL}/${short_url}`
+      const short_url = urls?.[0]?.short_url
+      const convertedUrl = `${SITE_URL}/${short_url}`
+      addUrl({ originalUrl: url, convertedUrl, date })
+      return convertedUrl
     } else {
-      // Si se proporciona un alias, usarlo en lugar de generar uno nuevo
-      if (alias) {
-        await supabase
-          .from("urls")
-          .insert([{ large_url: url, short_url: alias }])
-          .select()
-        console.log(
-          "La URL ha sido creada correctamente con el alias proporcionado"
-        )
-        return `${SITE_URL}/${alias}`
-      } else {
-        // Si no se proporciona un alias, generar uno nuevo
-        const short_url = generateShortUrl()
-        await supabase
-          .from("urls")
-          .insert([{ large_url: url, short_url: short_url }])
-          .select()
-        console.log("La URL ha sido creada correctamente")
-        return `${SITE_URL}/${short_url}`
-      }
+      const short_url = alias || generateShortUrl()
+      await supabase
+        .from("urls")
+        .insert([{ large_url: url, short_url }])
+        .select()
+      const convertedUrl = `${SITE_URL}/${short_url}`
+      addUrl({ originalUrl: url, convertedUrl, date })
+      return convertedUrl
     }
   } catch (error) {
     if (error instanceof Error) {
       console.error(error.message)
     }
-    // En caso de error, devuelve una respuesta adecuada
     return new Response("Error al procesar la solicitud", { status: 500 })
   }
 }
