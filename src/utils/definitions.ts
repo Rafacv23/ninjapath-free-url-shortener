90625ed7df568b508725a5ad3beac774ae17ff2e
Rafa Canosa <rafacv23@gmail.com>
2024-06-19 14:19:05 +0200

    new interfaces to url and zustand state

--- src/utils/definitions.ts
+++ src/utils/definitions.ts
@@ -1,3 +1,17 @@
 export type convertedUrl = {
   convertedUrl: string
 }
+
+export interface Url {
+  originalUrl: string
+  convertedUrl: string
+  date: string
+}
+
+export interface UrlsState {
+  urls: Url[]
+  addUrl: (url: Url) => void
+  removeUrl: (originalUrl: string) => void
+  clearUrls: () => void
+  updateUrls: (url: Url[]) => void
+}
