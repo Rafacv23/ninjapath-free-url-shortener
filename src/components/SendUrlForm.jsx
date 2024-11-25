e5f25c4bd43539138cd24dc4a6154716957a1390
Rafa Canosa <rafacv23@gmail.com>
2024-11-25 15:57:48 +0100

    styles

--- src/components/SendUrlForm.jsx
+++ src/components/SendUrlForm.jsx
@@ -4,6 +4,7 @@ import ConvertedUrl from "./ConvertedUrl"
 import { isValidUrl } from "../utils/isValidUrl"
 import { findExistingShortUrl } from "../lib/findExistingShortUrl"
 import { handleChange, handleChangeAlias } from "../utils/handleFunctions"
+import { X } from "lucide-react"
 
 export default function SendUrlForm() {
   const [url, setUrl] = useState("")
@@ -47,29 +48,51 @@ export default function SendUrlForm() {
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="url"
-          placeholder="Enter a long link here"
-          name="url"
-          id="url"
-          value={url}
-          onChange={() => handleChange(event, setUrl)}
-          required
-        />
+      <form onSubmit={handleSubmit} className="grid gap-4">
+        <span className="flex gap-2 items-center">
+          <input
+            type="url"
+            placeholder="*Enter a long link here"
+            name="url"
+            id="url"
+            value={url}
+            onChange={() => handleChange(event, setUrl)}
+            required
+            className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+          />
+          <button
+            onClick={() => setUrl("")}
+            className="bg-red-500 text-violet-50 shadow hover:bg-red-800 h-9 px-4 py-2 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+          >
+            <X />
+          </button>
+        </span>
         <small>Shorten a long URL</small>
-        <input
-          type="text"
-          min={2}
-          max={20}
-          placeholder="Enter alias"
-          name="alias"
-          id="alias"
-          value={alias}
-          onChange={() => handleChangeAlias(event, setAlias)}
-        />
+        <span className="flex gap-2 items-center">
+          <input
+            type="text"
+            min={2}
+            max={20}
+            placeholder="Enter alias"
+            name="alias"
+            id="alias"
+            value={alias}
+            onChange={() => handleChangeAlias(event, setAlias)}
+            className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+          />
+          <button
+            onClick={() => setAlias("")}
+            className="bg-red-500 text-violet-50 shadow hover:bg-red-800 h-9 px-4 py-2 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+          >
+            <X />
+          </button>
+        </span>
         <small>Customise your link</small>
-        <button type="submit" disabled={loading}>
+        <button
+          type="submit"
+          disabled={loading}
+          className="bg-violet-500 text-violet-50 shadow hover:bg-violet-800 h-9 px-4 py-2 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0"
+        >
           {loading ? (
             <span aria-busy="true">Generating your link...</span>
           ) : (
