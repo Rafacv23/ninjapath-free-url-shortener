107cb675d66f44931e28913660342754954d5e92
Rafa Canosa <rafacv23@gmail.com>
2024-11-25 15:57:23 +0100

    added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,15 @@
-import { defineConfig } from "astro/config"
-import vercel from "@astrojs/vercel/serverless"
-import react from "@astrojs/react"
+import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/serverless";
+import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
+import { SITE_URL } from "./src/utils/constants";
 
-import sitemap from "@astrojs/sitemap"
-import { SITE_URL } from "./src/utils/constants"
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
   output: "server",
   adapter: vercel(),
-  integrations: [react(), sitemap()],
-})
+  integrations: [react(), sitemap(), tailwind()]
+});
