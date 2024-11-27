bf2c8715b53a2bc5e83a68c4af63c08a96d3ec51
Rafa <rafacv23@gmail.com>
2024-11-27 18:08:19 +0100

    update: new color for the animation

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -25,7 +25,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
           background: useMotionTemplate`
         radial-gradient(
           ${visible ? radius + "px" : "0px"} circle at ${mouseX}px ${mouseY}px,
-          var(--blue-500),
+          var(--primary),
           transparent 80%
         )
       `,
