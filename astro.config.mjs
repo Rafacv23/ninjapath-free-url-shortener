6829c9321f4795b9778db57dfeacad70074ff182
Rafa <rafacv23@gmail.com>
2024-11-27 19:03:18 +0100

    update: configure astro internationalization

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
-import { defineConfig } from "astro/config";
-import vercel from "@astrojs/vercel/serverless";
-import react from "@astrojs/react";
-import sitemap from "@astrojs/sitemap";
-import { SITE_URL } from "./src/utils/constants";
+import { defineConfig } from "astro/config"
+import vercel from "@astrojs/vercel/serverless"
+import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
+import { SITE_URL } from "./src/utils/constants"
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
   output: "server",
   adapter: vercel(),
-  integrations: [react(), sitemap(), tailwind()]
-});
+  integrations: [react(), sitemap(), tailwind()],
+  i18n: {
+    defaultLocale: "en",
+    locales: ["es", "en"],
+    fallback: {
+      es: "en",
+    },
+  },
+})
