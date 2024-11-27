8483536459a500736d58795d92f6ea9c0a3f5ddc
Rafa <rafacv23@gmail.com>
2024-11-27 17:00:01 +0100

    new imports, and mapping

--- src/components/share/ShareRRSS.tsx
+++ src/components/share/ShareRRSS.tsx
@@ -13,57 +13,106 @@ import {
   WhatsappIcon,
   WhatsappShareButton,
 } from "react-share"
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalTrigger,
+} from "../ui/animated-modal"
+import { buttonVariants } from "../ui/button"
 
 export default function ShareRRSS({ convertedUrl }: { convertedUrl: string }) {
+  const socials = [
+    {
+      icon: EmailIcon,
+      text: "Email",
+      element: (
+        <EmailShareButton
+          url={convertedUrl}
+          className={buttonVariants({ variant: "default" })}
+        >
+          <EmailIcon size={32} round={true} /> Email
+        </EmailShareButton>
+      ),
+    },
+    {
+      icon: FacebookIcon,
+      text: "Facebook",
+      element: (
+        <FacebookShareButton
+          url={convertedUrl}
+          className={buttonVariants({ variant: "default" })}
+        >
+          <FacebookIcon size={32} round={true} /> Facebook
+        </FacebookShareButton>
+      ),
+    },
+    {
+      icon: LinkedinIcon,
+      text: "Linkedin",
+      element: (
+        <LinkedinShareButton
+          url={convertedUrl}
+          className={buttonVariants({ variant: "default" })}
+        >
+          <LinkedinIcon size={32} round={true} /> Linkedin
+        </LinkedinShareButton>
+      ),
+    },
+    {
+      icon: TwitterIcon,
+      text: "Twitter",
+      element: (
+        <TwitterShareButton
+          url={convertedUrl}
+          className={buttonVariants({ variant: "default" })}
+        >
+          <TwitterIcon size={32} round={true} /> Twitter
+        </TwitterShareButton>
+      ),
+    },
+    {
+      icon: WhatsappIcon,
+      text: "Whatsapp",
+      element: (
+        <WhatsappShareButton
+          url={convertedUrl}
+          className={buttonVariants({ variant: "default" })}
+        >
+          <WhatsappIcon size={32} round={true} /> Whatsapp
+        </WhatsappShareButton>
+      ),
+    },
+    {
+      icon: TelegramIcon,
+      text: "Telegram",
+      element: (
+        <TelegramShareButton
+          url={convertedUrl}
+          className={buttonVariants({ variant: "default" })}
+        >
+          <TelegramIcon size={32} round={true} /> Telegram
+        </TelegramShareButton>
+      ),
+    },
+  ]
+
   return (
-    <details className="marker:text-transparent">
-      <summary className="cursor-pointer bg-violet-500 text-violet-50 shadow hover:bg-violet-800 h-9 px-4 py-2 inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0">
+    <Modal>
+      <ModalTrigger>
         <Share2 />
-      </summary>
-      <ul className="flex flex-col gap-2 mt-4">
-        <li>
-          <a>
-            <EmailShareButton url={convertedUrl}>
-              <EmailIcon size={32} round={true} /> Email
-            </EmailShareButton>
-          </a>
-        </li>
-        <li>
-          <a>
-            <FacebookShareButton url={convertedUrl}>
-              <FacebookIcon size={32} round={true} /> Facebook
-            </FacebookShareButton>
-          </a>
-        </li>
-        <li>
-          <a>
-            <LinkedinShareButton url={convertedUrl}>
-              <LinkedinIcon size={32} round={true} /> Linkedin
-            </LinkedinShareButton>
-          </a>
-        </li>
-        <li>
-          <a>
-            <TwitterShareButton url={convertedUrl}>
-              <TwitterIcon size={32} round={true} /> Twitter
-            </TwitterShareButton>
-          </a>
-        </li>
-        <li>
-          <a>
-            <WhatsappShareButton url={convertedUrl}>
-              <WhatsappIcon size={32} round={true} /> Whatsapp
-            </WhatsappShareButton>
-          </a>
-        </li>
-        <li>
-          <a>
-            <TelegramShareButton url={convertedUrl}>
-              <TelegramIcon size={32} round={true} /> Telegram
-            </TelegramShareButton>
-          </a>
-        </li>
-      </ul>
-    </details>
+      </ModalTrigger>
+      <ModalBody>
+        <ModalContent>
+          <ul className="grid grid-cols-3 gap-2 mt-4">
+            {socials.map((rrss) => (
+              <li key={rrss.text}>
+                <a>{rrss.element}</a>
+              </li>
+            ))}
+          </ul>
+        </ModalContent>
+      </ModalBody>
+    </Modal>
   )
 }
