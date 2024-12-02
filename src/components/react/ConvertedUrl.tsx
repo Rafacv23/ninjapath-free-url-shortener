b6bfd6f68b213236ec6e7b35fd9ef74b9b64bee5
Rafa Canosa <rafacv23@gmail.com>
2024-12-02 17:05:01 +0100

    update ConvertedUrl.tsx

--- src/components/react/ConvertedUrl.tsx
+++ src/components/react/ConvertedUrl.tsx
@@ -1,7 +1,13 @@
 import { copyUrlToClipboard } from "@/actions/copyUrlToClipboard"
 import type { convertedUrl } from "@/utils/definitions"
 import { Copy, MoveUpRight, Share } from "lucide-react"
-import { Card, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 import {
   Tooltip,
   TooltipContent,
@@ -15,10 +21,11 @@ const ConvertedUrl = ({ convertedUrl }: convertedUrl) => {
   const buttons = [
     {
       icon: Copy,
-      text: "Copy",
+      text: "Copy link",
       element: (
         <Button onClick={() => copyUrlToClipboard(convertedUrl)}>
-          <Copy />
+          <Copy className="h-[1.2rem] w-[1.2rem]" />{" "}
+          <span className="hidden sm:block">Copy link</span>
         </Button>
       ),
     },
@@ -30,21 +37,34 @@ const ConvertedUrl = ({ convertedUrl }: convertedUrl) => {
   ]
 
   return (
-    <Card>
-      <CardHeader>
+    <Card className="flex flex-row items-center justify-between p-4 max-w-4xl">
+      <CardHeader className="xl:p-0">
         <CardTitle>
           <a
             href={convertedUrl}
-            className="flex items-center gap-2 hover:text-primary hover:transition-colors"
+            className="inline-flex items-baseline font-medium leading-tight text-slate-100 hover:text-primary focus-visible:text-primary group/link text-base sm:text-xl"
             target="_blank"
             rel="noreferrer"
           >
-            {convertedUrl} <MoveUpRight />
+            {convertedUrl}
+            <svg
+              className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10.5253 5.49475L10.5206 7.49475L15.0782 7.50541L5.47473 17.0896L6.88752 18.5052L16.5173 8.89479L16.5065 13.5088L18.5065 13.5134L18.5253 5.51345L10.5253 5.49475Z"
+                fill="currentColor"
+              />
+            </svg>
           </a>
         </CardTitle>
       </CardHeader>
-      <CardFooter>
-        <ul>
+      <CardContent className="p-0">
+        <ul className="flex flex-row items-center gap-2">
           {buttons.map((button) => (
             <TooltipProvider key={button.text}>
               <Tooltip>
@@ -56,7 +76,7 @@ const ConvertedUrl = ({ convertedUrl }: convertedUrl) => {
             </TooltipProvider>
           ))}
         </ul>
-      </CardFooter>
+      </CardContent>
     </Card>
   )
 }
