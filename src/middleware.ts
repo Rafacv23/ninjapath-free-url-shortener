5e86feb15c614093d6293f410e2e871ab5970356
Rafa Canosa <rafacv23@gmail.com>
2024-12-04 10:49:41 +0100

    update: new protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,11 @@
-import { clerkMiddleware } from "@clerk/astro/server"
+import { clerkMiddleware, createRouteMatcher } from "@clerk/astro/server"
 
-export const onRequest = clerkMiddleware()
+const isProtectedRoute = createRouteMatcher([`/archive(.*)`])
+
+export const onRequest = clerkMiddleware((auth, context) => {
+  const { redirectToSignIn, userId } = auth()
+
+  if (!userId && isProtectedRoute(context.request)) {
+    return redirectToSignIn()
+  }
+})
