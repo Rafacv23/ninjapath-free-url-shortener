4515992ba04028cde42ab48cea859afe7d99f473
Rafa Canosa <rafacv23@gmail.com>
2024-12-04 12:16:52 +0100

    fix: error handling

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,8 +5,6 @@ const isProtectedRoute = createRouteMatcher([`/archive(.*)`])
 export const onRequest = clerkMiddleware((auth, context) => {
   const { redirectToSignIn, userId } = auth()
 
-  context.locals.currentUser = auth().userId
-
   if (!userId && isProtectedRoute(context.request)) {
     return redirectToSignIn()
   }
