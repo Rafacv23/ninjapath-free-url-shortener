912e92a655ba6ef3cfcf137bf0452d8f8de55d86
Rafa Canosa <rafacv23@gmail.com>
2024-12-09 15:38:25 +0100

    hotfix: replaced development url for production

--- src/services/fetchLargeUrl.ts
+++ src/services/fetchLargeUrl.ts
@@ -1,19 +1,17 @@
 import { supabase } from "@/lib/supabase"
+import { SITE_URL } from "@/utils/constants"
 
 // check if the shortUrl is already in the database (with alias or without)
 export default async function fetchLargeUrl(
   url: string | undefined
 ): Promise<string> {
   try {
-    const response = await fetch(
-      `http://localhost:4321/api/url/short/${url}.json`,
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      }
-    )
+    const response = await fetch(`${SITE_URL}/api/url/short/${url}.json`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
     if (!response.ok) {
       const errorData = await response.json()
       throw new Error(
